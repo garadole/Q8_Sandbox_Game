dc3827d4aca462dfb67c34db0c26a67ba5b68e55
Yooooos <72731760+Yooooos@users.noreply.github.com>
2024-07-23 16:33:20 +0300

    Initial push of project
    Setting Basic 3D Project Platform

---
+++ Assets/Code/Test_Behavior.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_Behavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello guys");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
